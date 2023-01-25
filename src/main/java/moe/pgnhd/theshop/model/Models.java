c5b2bb690bb2516b9567206ce8013e4385371ab0
Martin Ransbach <ransbachm@gmail.com>
2023-01-25 17:39:56 +0100

    Small refactor & javadoc

--- src/main/java/moe/pgnhd/theshop/model/Models.java
+++ src/main/java/moe/pgnhd/theshop/model/Models.java
@@ -1,11 +1,9 @@
 package moe.pgnhd.theshop.model;
 
-import moe.pgnhd.theshop.Verwaltung;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -15,19 +13,18 @@ public class Models {
     private static Logger LOG = LoggerFactory.getLogger(Models.class);
 
     /**
-     *
-     * @param rs ResultSet that contains the "one" and "many" tables joined
+     * Returns multiple "one to many" relations
+     * @param rs ResultSet that contains the "one" and "many" tables joined. It will be consumed.
+     *           It needs to be ordered by at least the "one"-side's pk asc.
      * @param one Binding class of "one" table
      * @param many Binding class of "many" table
      * @param container_name_one Container on "one" side that will receive "many"-side objects
      * @param table_one Name of the "many" table
      * @param pk_one Name of the pk on the "one" side
-     * @apiNote ResultSet needs to be ordered by at least the "one"-side's pk
      * @return A List containing every "one-many" relation found
-     * @param <T>
      */
 
-    public static <T> List<T> multiple_one_to_many(ResultSet rs, Class one, Class many, String container_name_one,
+    public static <T extends ResultSetConstructable> List<T> multiple_one_to_many(ResultSet rs, Class one, Class many, String container_name_one,
                                                    String table_one, String pk_one) {
         List<T> res = new ArrayList<T>();
         int i=0;
@@ -54,16 +51,15 @@ public class Models {
     }
 
     /**
-     * Sets sensible default values for the "one"-side table name and pk
-     * @param rs ResultSet that contains the "one" and "many" tables joined
-     * @param one Binding class of "one" table
-     * @param many Binding class of "many" table
+     * Returns multiple "one to many" relations
+     * @param rs ResultSet that contains the "one" and "many" tables joined. It will be consumed.
+     *           It needs to be ordered by at least the "one"-side's pk.
+     * @param one Binding class of "one" table needs to be ResultSetConstructable
+     * @param many Binding class of "many" table to be ResultSetConstructable
      * @param container_name_one Container on "one" side that will receive "many"-side objects
-     * @apiNote ResultSet needs to be ordered by at least the "one"-side's pk
      * @return A List containing every "one-many" relation found
-     * @param <T>
      */
-    public static <T> List<T> multiple_one_to_many(ResultSet rs, Class one, Class many, String container_name_one) {
+    public static <T extends ResultSetConstructable> List<T> multiple_one_to_many(ResultSet rs, Class one, Class many, String container_name_one) {
         return multiple_one_to_many(rs, one, many, container_name_one, one.getSimpleName(), "id");
     }
 
