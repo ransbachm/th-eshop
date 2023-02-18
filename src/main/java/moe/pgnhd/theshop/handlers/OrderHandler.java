552d451dcfc398022fb23fce9aef0245f84f6977
ransbachm <ransbachm@gmail.com>
2023-02-18 21:58:23 +0100

    Fix order page (#17)
    Should have already been fixed in #10

--- src/main/java/moe/pgnhd/theshop/handlers/OrderHandler.java
+++ src/main/java/moe/pgnhd/theshop/handlers/OrderHandler.java
@@ -1,8 +1,8 @@
 package moe.pgnhd.theshop.handlers;
 
 import moe.pgnhd.theshop.Main;
-import moe.pgnhd.theshop.model.User;
 import moe.pgnhd.theshop.Util;
+import moe.pgnhd.theshop.model.User;
 import spark.Request;
 import spark.Response;
 
@@ -11,7 +11,8 @@ import java.util.Map;
 public class OrderHandler {
     public static String handleGetOrders(Request req, Response res) {
         Map<String, Object> model = Util.getModel(req);
-        model.put("orders", Main.management.getOrders());
+        User user = req.attribute("user");
+        model.put("orders", Main.management.getMyOrders(user));
 
         return Main.render("my/orders", model);
     }
