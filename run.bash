82f50a0e14ebe91bf79552bfdcf713c32287141a
Martin Ransbach <ransbachm@gmail.com>
2023-03-19 22:06:10 +0100

    Fixes

---
+++ run.bash
@@ -0,0 +1,1 @@
+mvn exec:java -Dexec.mainClass=moe.pgnhd.theshop.Main
--- src/main/java/moe/pgnhd/theshop/Mail.java
+++ src/main/java/moe/pgnhd/theshop/Mail.java
@@ -10,7 +10,6 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
